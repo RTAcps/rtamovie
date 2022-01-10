94cd1e02190d13bd1340a33bed6fe9d914bb0bc6
RTAcps <rodrigoandrade20@yahoo.com.br>
2022-01-10 16:25:26 -0300

    Project created

---
+++ backend/src/main/java/com/humadaroflix/rtamovie/RtamovieApplication.java
@@ -0,0 +1,13 @@
+package com.humadaroflix.rtamovie;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RtamovieApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RtamovieApplication.class, args);
+	}
+
+}
