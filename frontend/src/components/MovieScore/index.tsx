16b22c9073c05857b6352b430762e34674414593
RTAcps <rodrigoandrade20@yahoo.com.br>
2022-01-14 19:13:56 -0300

    Show score

--- frontend/src/components/MovieScore/index.tsx
+++ frontend/src/components/MovieScore/index.tsx
@@ -1,16 +1,18 @@
 import MovieStars from "../MovieStars";
 import "./styles.css";
 
-function MovieScore() {
-  const score = 3.5;
-  const count = 13;
+type Props = {
+  score: number;
+  count: number;
+};
 
+function MovieScore({ score, count }: Props) {
   return (
     <div className="rtamovie-score-container">
       <p className="rtamovie-score-value">
         {score > 0 ? score.toFixed(1) : "-"}
       </p>
-      <MovieStars />
+      <MovieStars score={score} />
       <p className="rtamovie-score-count">{count} avaliações</p>
     </div>
   );
