2eb8b8f326b557c370d92950b0b6c713d14058e3
RTAcps <rodrigoandrade20@yahoo.com.br>
2022-01-14 17:23:45 -0300

    First request

--- frontend/src/pages/Listing/index.tsx
+++ frontend/src/pages/Listing/index.tsx
@@ -1,11 +1,16 @@
+import axios from "axios";
+import { BASE_URL } from "utils/requests";
 import MovieCard from "../../components/MovieCard";
 import Pagination from "../../components/Pagination";
 
 function Listing() {
+  axios.get(`${BASE_URL}/movies?size=12&page=0`).then((response) => {
+    console.log(response.data);
+  });
+
   return (
     <>
       <Pagination />
-
       <div className="container">
         <div className="row">
           <div className="col-sm-6 col-lg-4 col-xl-3 mb-3">
